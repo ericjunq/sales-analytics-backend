99b6f7b3bd019e1c4dce66eaed4b092bb9918604
Eric Junqueira <junqueiraeric576@gmail.com>
2026-04-14 00:07:56 -0300

    a

--- routers/auth_routers.py
+++ routers/auth_routers.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, HTTPException, Depends
 from dependencies import get_db
-from security.security import criptografar_senha, verificar_senha
+from security.security import criptografar_senha, verificar_senha, criar_access_token, criar_refresh_token, get_current_empresa
 from schemas import EmpresaResponse, EmpresaSchema
 from sqlalchemy.orm import Session
 from models import Empresa
+from fastapi.security import OAuth2PasswordRequestForm
 
 auth_router = APIRouter(prefix='/empresa', tags=['empresa'])
 
-@auth_router.post('/cadastrar_empresa', response_model=EmpresaResponse)
+@auth_router.post('/cadastro', response_model=EmpresaResponse)
 async def cadastrar_empresa(
     empresaschema: EmpresaSchema,
     db: Session = Depends(get_db)
@@ -39,3 +40,31 @@ async def cadastrar_empresa(
     db.refresh(empresa)
 
     return empresa
+
+@auth_router.post('/login')
+async def login(
+    dados: OAuth2PasswordRequestForm = Depends(), 
+    db: Session = Depends(get_db)
+):
+    empresa = db.query(Empresa).filter(
+        Empresa.email == dados.username
+    ).first()
+    if empresa is None:
+        raise HTTPException(status_code=404, detail='Email não encontrado')
+    
+    if not verificar_senha(dados.password, empresa.senha_hash):
+        raise HTTPException(status_code=401, detail='Senha incorreta')
+    
+    access_token = criar_access_token(
+        data = {'sub': dados.username, 'type': 'access'}
+    )
+
+    refresh_token = criar_refresh_token(
+        data = {'sub': dados.username, 'type': 'refresh'}
+    )
+
+    return {
+        'access_token': access_token,
+        'refresh_token': refresh_token,
+        'token_type': 'bearer'
+    }
--- security/security.py
+++ security/security.py
@@ -1,4 +1,14 @@
 from pwdlib import PasswordHash
+from jose import jwt, JWTError
+from settings import settings
+from datetime import datetime, timezone, timedelta
+from fastapi.security import OAuth2PasswordBearer
+from dependencies import get_db
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from models import Empresa
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl='/empresa/login')
 
 password_hash = PasswordHash.recommended()
 
@@ -7,3 +17,52 @@ def criptografar_senha(senha: str)-> str:
 
 def verificar_senha(senha: str, senha_hash: str)->bool: 
     return password_hash.verify(senha, senha_hash)
+
+def criar_access_token(dados: dict):
+    to_encode = dados.copy()
+    expires = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expires_minutes)
+    to_encode.update({'exp': expires})
+
+    access_token = jwt.encode(
+        to_encode,
+        settings.secret_key,
+        settings.algorithm
+    )
+
+    return access_token
+
+def criar_refresh_token(dados:dict):
+    to_encode = dados.copy()
+    expires = datetime.now(timezone.now) + timedelta(days=settings.refresh_token_expires_days)
+    to_encode.update({'exp': expires})
+
+    refresh_token = jwt.encode(
+        to_encode,
+        settings.secret_key,
+        settings.algorithm
+    )
+
+    return refresh_token
+
+def get_current_empresa(
+        token: str = Depends(oauth_scheme), 
+        db: Session = Depends(get_db)
+):
+    try:
+        payload = jwt.decode(
+            token,
+            settings.secret_key,
+            [settings.algorithm]
+        )
+        email = payload.get('sub')
+        if email is None:
+            raise HTTPException(status_code=401, detail='Token inválido')
+        
+    except JWTError:
+        raise HTTPException(status_code=401, detail='Token inválido')
+    
+    empresa = db.query(Empresa).filter(Empresa.email == email).first()
+    if empresa is None:
+        raise HTTPException(status_code=401, detail='Empresa não encontrada')
+    
+    return empresa
